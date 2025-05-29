b316c8111a284dd2eb7210973d1930ece41187f6
freebirdyeah <mr.shaanchopra@gmail.com>
2025-05-30 00:58:20 +0530

    added some vector funcs

--- src/vector.c
+++ src/vector.c
@@ -1,3 +1,63 @@
 #include "vector.h"
+#include <math.h>
 
-Vec2 vec2GetZero() { return (Vec2) { 0.0f, 0.0f }; }
+
+TGAPI Vec2 vec2GetZero() {
+    return (Vec2) { 0.0f, 0.0f };
+}
+
+TGAPI Vec2 vec2Add(Vec2 v1, Vec2 v2) {
+    return (Vec2) {(v1.x + v2.x), (v1.y + v2.y) };
+}
+
+TGAPI Vec2 vec2Sub(Vec2 v1, Vec2 v2) {
+    return (Vec2) {v1.x - v2.x,v1.y - v2.y };
+}
+
+TGAPI Vec2 vec2ScalarAdd(Vec2 v1, float scalar) {
+    return (Vec2) {(v1.x + scalar),(v1.y + scalar) };
+}
+
+TGAPI Vec2 vec2ScalarSub(Vec2 v1, float scalar) {
+    return (Vec2) {(v1.x - scalar),(v1.y - scalar) };
+}
+
+TGAPI Vec2 vec2Scale(Vec2 v1, float scalar) {
+    return (Vec2) {(v1.x * scalar),(v1.y * scalar) };
+}
+
+TGAPI float vec2Dot(Vec2 v1, Vec2 v2) {
+    return (v1.x * v2.x) + (v1.y * v2.y);
+}
+
+TGAPI float vec2Cross(Vec2 v1, Vec2 v2) {
+    return (v1.x * v2.y) - (v1.y * v2.x);
+}
+
+TGAPI float vec2Length(Vec2 v1) {
+    return sqrtf(v1.x * v1.x + v1.y * v1.y);
+}
+
+TGAPI float vec2LengthSquared(Vec2 v1) {
+    return (v1.x * v1.x + v1.y * v1.y);
+}
+
+TGAPI Vec2 vec2Normalize(Vec2 v1) {
+    float len = vec2Length(v1);
+    return (Vec2) {(v1.x / len), (v1.y / len) };
+}
+
+TGAPI float vec2DistanceFromPoint(Vec2 v1, Vec2 v2) {
+    Vec2 pointer_vector = vec2Sub(v1, v2);
+    return vec2Length(pointer_vector);
+}
+
+// Ternary operations should be efficient at -O3 or -O3
+// RELEVANT: https://stackoverflow.com/questions/427477/fastest-way-to-clamp-a-real-fixed-floating-point-value
+TGAPI Vec2 vec2Clamp(Vec2 v1, float min, float max) {
+    return (Vec2) {
+        v1.x > min ? v1.x < max ? v1.x : max : min,
+        v1.y > min ? v1.y < max ? v1.y : max : min,
+      };
+}
+
